83c8410bab59a2c8b0e75dc293339b8f944e5cd9
unknown <1754350460@qq.com>
2021-11-16 22:03:48 +0800

    fixed integration bug

--- 6-recursion/integration.c
+++ 6-recursion/integration.c
@@ -5,15 +5,19 @@
 #include <malloc.h>
 #include <math.h>
 
-double calculate(double coef[], int coefNumber, double x, int p);
-double calculateInt(int coef[], int coefNumber, double x, int p);
-double simpsonIntegration(int coef[], int coefNumber, double left, double right, int p);
-double simpsonMethod(int coef[], int coefNumber, double left, double right, int p);
+int* coef;
+double* integrationCoef;
+
+double calculate(int coefNumber, double x);
+double calculateInt(int coefNumber, double x, int p);
+double simpsonIntegration(int coefNumber, double left, double right, int p, double deviation);
+double simpsonMethod(int coefNumber, double left, double right, int p);
 
 int main() {
     int n, p;
+    double deviation = 1e-6;
     scanf("%d %d", &n, &p);
-    int* coef = malloc((n + 1) * sizeof(int));
+    coef = malloc((n + 1) * sizeof(int));
     for (int i = 0; i <= n; i++) {
         scanf("%d", &coef[i]);
     }
@@ -25,18 +29,18 @@ int main() {
 
     if (p == 1) {
         // p == 1时我们用常规积分方法
-        double* integrationCoef = malloc((n + 1) * sizeof(double));
+        integrationCoef = malloc((n + 1) * sizeof(double));
         double temp = n;
         for (int i = n; i >= 0; i--) {
             integrationCoef[i] = coef[i] / (temp + 1);
             temp--;
         }
         // F(b) - F(a)
-        ans = calculate(integrationCoef, n, b, p) - calculate(integrationCoef, n, a, p);
+        ans = calculate(n, b) - calculate(n, a);
         free(integrationCoef);
     } else {
         // adaptive simpson's method
-        ans = simpsonIntegration(coef, n, a, b, p);
+        ans = simpsonIntegration(n, a, b, p, deviation);
     }
 
     printf("%lf", ans);
@@ -44,19 +48,19 @@ int main() {
     return 0;
 }
 
-double calculate(double coef[], int coefNumber, double x, int p) {
+double calculate(int coefNumber, double x) {
     double ans = 0.0;
     double product = x;
 
     for (int i = 0; i <= coefNumber; i++) {
-        ans += coef[i] * product;
+        ans += integrationCoef[i] * product;
         product *= x;
     }
 
-    return pow(ans, p);
+    return ans;
 }
 
-double calculateInt(int coef[], int coefNumber, double x, int p) {
+double calculateInt(int coefNumber, double x, int p) {
     double ans = 0.0;
     double product = 1;
 
@@ -68,19 +72,20 @@ double calculateInt(int coef[], int coefNumber, double x, int p) {
     return pow(ans, p);
 }
 
-double simpsonIntegration(int coef[], int coefNumber, double left, double right, int p) {
+double simpsonIntegration(int coefNumber, double left, double right, int p, double deviation) {
     double middle = (right + left) / 2.0;
     double sl = simpsonMethod(coef, coefNumber, left, middle, p);
     double sr = simpsonMethod(coef, coefNumber, middle, right, p);
     double s = simpsonMethod(coef, coefNumber, left, right, p);
-    if (fabs(sl + sr - s) <= 15e-8) {
+    if (fabs(sl + sr - s) <= 15 * deviation) {
         return sl + sr +(sl + sr - s) / 15;
     } else {
-        return simpsonIntegration(coef, coefNumber, left, middle, p) + simpsonIntegration(coef, coefNumber, middle, right, p);
+        // 误差除2
+        return simpsonIntegration(coef, coefNumber, left, middle, p, deviation / 2) + simpsonIntegration(coef, coefNumber, middle, right, p, deviation / 2);
     }
 }
 
-double simpsonMethod(int coef[], int coefNumber, double left, double right, int p) {
+double simpsonMethod(int coefNumber, double left, double right, int p) {
     // 用辛普森公式计算的值
     double middle = (right + left) / 2.0;
     double ans1 = 4 * calculateInt(coef, coefNumber, middle, p);
