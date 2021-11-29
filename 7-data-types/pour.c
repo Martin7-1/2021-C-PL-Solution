3f36a58dc0ff741c6d122900b8df6078f365ceea
unknown <1754350460@qq.com>
2021-11-29 23:26:35 +0800

    update pour.c

--- 7-data-types/pour.c
+++ 7-data-types/pour.c
@@ -4,51 +4,35 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-long actual[5];
-long expect[5];
-int step = 0;
-bool canPour = false;
+long a0;
+long b0;
+long c0;
+int k;
 
-void pour(int k);
+void pour(int step, long remainA, long remainB, long remainC);
 
 int main() {
-    int k;
     // 本题需要用long
-    long va, vb, vc, a, b, c, a0, b0, c0;
+    long va, vb, vc, a, b, c;
     scanf("%d %ld %ld %ld %ld %ld %ld %ld %ld %ld", &k, &va, &vb, &vc, &a, &b, &c, &a0, &b0, &c0);
-    actual[0] = a;
-    actual[1] = b;
-    actual[2] = c;
-    expect[0] = a0;
-    expect[1] = b0;
-    expect[2] = c0;
 
     if (a + b + c != a0 + b0 + c0) {
         printf("No");
     } else {
-        pour(k);
+        pour(0, a, b, c);
     }
 
-    if (canPour) {
-        printf("Yes");
-    } else {
-        printf("No");
-    }
+    return 0;
 }
 
-void pour(k) {
-    if (actual[0] == expect[0] && actual[1] == expect[1] && actual[2] == expect[2] && step <= k) {
-        canPour = true;
+void pour(int step, long remainA, long remainB, long remainC) {
+    if (step >= k) {
+        printf("No");
         return;
-    } else if (step > k) {
+    } else if (remainA == a0 && remainB == b0 && remainC == c0) {
+        printf("Yes");
         return;
     }
 
-    // 递归尝试所有办法
-    if (actual[0] < expect[0]) {
-        actual[1] = 0;
-        pour();
-    } else if (actual[0] > expect[0]) {
 
-    }
 }
